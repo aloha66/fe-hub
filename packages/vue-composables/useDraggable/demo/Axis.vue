3f3fe53b8b6ba732093a3c73a047993227a58ae5
aloha66 <lzcbruce@live.com>
2024-01-17 16:09:28 +0800

    feat(darg): 添加axis demo

---
+++ packages/vue-composables/useDraggable/demo/Axis.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useDraggable } from '..'
+import Wrapper from './Wrapper.vue'
+
+const el = ref<HTMLElement | null>(null)
+
+const { style: styleX } = useDraggable(el, { axis: 'x' })
+
+const elY = ref<HTMLElement | null>(null)
+
+const { style: styleY } = useDraggable(elY, { axis: 'y' })
+</script>
+
+<template>
+  <div ref="el" :style="styleX">
+    <Wrapper>
+      axisX
+    </Wrapper>
+  </div>
+
+  <div ref="elY" :style="styleY">
+    <Wrapper>
+      axisY
+    </Wrapper>
+  </div>
+</template>
--- packages/vue-composables/useDraggable/index.md
+++ packages/vue-composables/useDraggable/index.md
@@ -1,11 +1,14 @@
 <script setup>
 import OffestParent from "./demo/OffestParent.vue"
 import AllowAnyClick from "./demo/AllowAnyClick.vue"
+import Axis from "./demo/Axis.vue"
 </script>
 
 ## useDraggable
 
 ## AllowAnyClick
 <AllowAnyClick/>
+## Axis
+<Axis/>
 ## OffestParent
 <OffestParent/>
