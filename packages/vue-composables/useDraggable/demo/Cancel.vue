17b699865a64d5a9198ce1e73ba779e07e453447
aloha66 <lzcbruce@live.com>
2024-01-20 10:32:16 +0800

    feat(drag): 添加cancal的demo

---
+++ packages/vue-composables/useDraggable/demo/Cancel.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useDraggable } from '..'
+import Wrapper from './Wrapper.vue'
+
+const el = ref<HTMLElement | null>(null)
+
+const { style: styleX } = useDraggable(el, { cancel: '.not_move' })
+</script>
+
+<template>
+  <div ref="el" :style="styleX">
+    <Wrapper>
+      cancel
+      <div class="not_move">can't move here</div>
+    </Wrapper>
+  </div>
+</template>
--- packages/vue-composables/useDraggable/index.md
+++ packages/vue-composables/useDraggable/index.md
@@ -3,10 +3,13 @@ import OffestParent from "./demo/OffestParent.vue"
 import AllowAnyClick from "./demo/AllowAnyClick.vue"
 import Axis from "./demo/Axis.vue"
 import Bound from "./demo/Bound.vue"
+import Cancel from "./demo/Cancel.vue"
 
 </script>
 
 ## useDraggable
+## Cancel
+<Cancel/>
 ## Bound
 <Bound/>
 ## Axis
